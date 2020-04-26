c23a588f7b770e70c5ef89b9f40ed2f0df0fac0b
ihassanjavaid <great.maliks@gmail.com>
2020-04-26 16:23:17 +0500

    Revert "Delete"
    This reverts commit e3cb912166038205efb40c43c55b0f56ea9ddafa.

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:convo/screens/converstation_screen.dart';
+import 'package:convo/screens/starter_topics_screen.dart';
+import 'package:convo/screens/upgrade_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() => runApp(Convo());
+
+class Convo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Convo",
+      debugShowCheckedModeBanner: false,
+      initialRoute: RouteDecider.id,
+      routes: {
+        RouteDecider.id: (context) => RouteDecider(),
+        ConversationScreen.id: (context) => ConversationScreen(),
+        StarterTopicsScreen.id: (context) => StarterTopicsScreen(),
+        UpgradeScreen.id: (context) => UpgradeScreen(),
+      },
+    );
+  }
+}
+
+// TODO fix the Android app Icon through ImageAsset
+class RouteDecider extends StatelessWidget {
+  static const String id = 'route_decider';
+
+  void autoLogin(context) async {
+    final SharedPreferences pref = await SharedPreferences.getInstance();
+    final String userId = pref.getString('email');
+
+    if (userId != null) {
+      print('Logged in automatically');
+      //Navigator.pushReplacementNamed(context, Index.id);
+      return;
+    } else {
+      print('First time sign in');
+      //Navigator.pushReplacementNamed(context, Login.id);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    autoLogin(context);
+    return Container();
+  }
+}
---
+++ lib/screens/upgrade_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UpgradeScreen extends StatelessWidget {
+  static const String id = 'upgrade_screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
