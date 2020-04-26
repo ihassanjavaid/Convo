d6b43e7e5900446f73dc5c2c95650bb27909e9dc
elvin_arrow <m.talhabinmansoor@gmail.com>
2020-04-26 16:17:28 +0500

    Starting Template

---
+++ lib/screens/starter_topics_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StarterTopicsScreen extends StatelessWidget {
+  static const String id = 'starter_topics_screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
