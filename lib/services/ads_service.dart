ae9334d168145068ec13487205183ffb48a99956
ihassanjavaid <great.maliks@gmail.com>
2020-04-26 15:20:17 +0500

    Update ads_service.dart

--- lib/services/ads_service.dart
+++ lib/services/ads_service.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_admob/firebase_admob.dart';
 
-// Will be updated when we recieve these, DONDE ones here
 const String appIDAndroid = "ca-app-pub-2167282335700083~6409081517";
 const String interstitialIDAndroid = "ca-app-pub-2167282335700083/1902860291";
 const String bannerIDAndroid = "ca-app-pub-2167282335700083/6572733049";
